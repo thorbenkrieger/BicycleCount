b5d4d3eed5b945e3e651945634c413cff0e4a5f5
Thorben Krieger <thorben.krieger@gmail.com>
2025-03-19 14:22:24 +0100

    Hello world example

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functions_framework as functions
- 
-functions.http('helloHttp', (req, res) => {
-   res.send("Hello world")
-);}
+import functions_framework
+
+
+@functions_framework.cloud_event
+def hello_world(event):
+    """Cloud Event Function.
+    Args:
+        event: Cloud event for the function trigger
+    """
+    print("Hello, stdout!")
