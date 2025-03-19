6b6984e08af4ad726bd3bb0a4da1755746c6747c
Thorben Krieger <thorben.krieger@gmail.com>
2025-03-19 14:10:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import functions_framework as functions
  
  functions.http('helloHttp', (req, res) => {
 
+    res.send("Hello world");
 
-    request_json = req.get_json(silent=True)
-    request_args = req.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    res.send('Hello {}!'.format(name));
-
-   );}
+ );}
